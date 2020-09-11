a3147be209cf9f769fbdafeab423679fdeb88103
Vadim <48584192+ChinChoCho@users.noreply.github.com>
2020-09-12 00:34:22 +0300

    Finished header using dynamic adaptive in menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
+
+"use strict";
 // $(document).ready(function () {
 //    $(".icon-menu").click(function (event) {
 //       $(".icon-menu").toggleClass("icon-menu_active");
 //    });
 // });
 
-function addToggleElement(elementClass, toggleClass) {
+function add_toggle_element(elementClass, elementsArray, toggleClass) {
    let element = document.querySelector("." + elementClass);
    element.addEventListener("click", function () {
       element.classList.toggle(toggleClass);
+      for (let i = 0; i < elementsArray.length; i++) {
+         let element = document.querySelector("." + elementsArray[i]);
+         element.classList.toggle(toggleClass);
+      }
    });
 }
 
-addToggleElement("icon-menu", "_active");
+
+add_toggle_element("icon-menu", ["menu__body"], "_active");
