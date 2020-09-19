fb6cc9d2e54bc6afe8acef6fd757a1b9d5a0c59d
Vadim <48584192+ChinChoCho@users.noreply.github.com>
2020-09-19 20:38:52 +0300

    Complited sidebar with click error on mobile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
 "use strict";
+
 // $(document).ready(function () {
 //    $(".icon-menu").click(function (event) {
 //       $(".icon-menu").toggleClass("icon-menu_active");
@@ -16,5 +17,47 @@ function add_toggle_element(elementClass, elementsArray, toggleClass) {
    });
 }
 
+function add_hover_elements(elementsClass, toggleClass) {
+   let elements = document.querySelectorAll("." + elementsClass);
+   for (let i = 0; i < elements.length; i++) {
+      elements[i].addEventListener("mouseenter", function () {
+         if (isMobile()) {
+            let childElements = document.querySelectorAll("." + elementsClass + ">a");
+            childElements[i].addEventListener("click", function (e) {
+               childElements[i].parentElement.classList.toggle(toggleClass);
+               e.preventDefault();
+            });
+         }
+         else {
+            elements[i].classList.add(toggleClass);
+         }
+      });
+      elements[i].addEventListener("mouseleave", function () {
+         if (isMobile()) {
+
+         }
+         else {
+            elements[i].classList.remove(toggleClass);
+         }
+      });
+   }
+}
+
+function isMobile() {
+   let width = document.body.clientWidth;
+   if (width < 992) {
+      return true;
+   }
+   else {
+      return false;
+   }
+}
+
 add_toggle_element("icon-menu", ["icon-menu", "menu__body"], "_active");
-add_toggle_element("menu-page__burger", ["menu-page__lines"], "_active");
+add_toggle_element("menu-page__burger", ["menu-page__lines", "menu-page__body"], "_active");
+
+add_hover_elements("menu-page__link_parent", "_active");
+
+
+// add_hover_elements("menu-page__link_parent", "_active");
+// console.log(isMobile());
